45d46727329f3a51a8ee26599e40ae36f2b7b72f
Sushmitha0805 <69563297+Sushmitha0805@users.noreply.github.com>
2020-08-16 11:56:11 +0530

    Resolved conflict

--- arithmatic-computation.sh
+++ arithmatic-computation.sh
@@ -1,10 +1,11 @@
-
 #!/bin/bash -x
+
 declare -A sorting
 
 read -p "Enter the value  of a = " vara
 read -p "Enter the value  of b = " varb
 read -p "Enter the value  of c = " varc
+
 exp1=$(($vara+$varb*$varc))
 	echo " a+b*c = " $exp1
 exp2=$(($vara*$varb+$varc))
